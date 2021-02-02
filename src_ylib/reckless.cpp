ce587045fd19125c5f8f64d4b19d5274cdb8ffd4
ktchow1 <ktchow1@yahoo.com.hk>
2021-02-03 02:01:40 +0800

    custom header for reckless log

--- src_ylib/reckless.cpp
+++ src_ylib/reckless.cpp
@@ -111,14 +111,40 @@ void test_reckless_custom_argument()
 // 3. self-defined severity
 //
 // Headers are different from arguments.
-// Headers are predefined. Headers can be customized POD.
-// Arguments can be variadic. Arguments can also be customized POD.
+// Headers do not support format specifier.
+// Arguments do. 
 //
 // Remark :
 // 1. There is problem for reckless-log to handle std::string, but char array is OK. 
 // 2. There is no tutorial about inheriting reckless::basic_log, we discovered it 
 //    by tracing code in class policy_formatter. How is this possible?
 // 
+
+template<std::uint32_t M, std::integral T> // M = number of digits, N = number to be written on buffer
+void write_digits(char* pc, T N) 
+{
+    for(std::uint32_t m=0; m!=M; ++m)
+    {
+        pc[M-1-m] = '0' + N%10;
+        N = N/10;
+    }
+}
+
+struct custom_header0
+{
+    // Need to write integer into ASCII array
+    inline bool format(reckless::output_buffer* buffer) 
+    {
+        char* pc = buffer->reserve(sizeof(std::uint32_t)+2); 
+        pc[0] = '[';
+        write_digits<sizeof(std::uint32_t)>(pc+1, x);
+        pc[5] = ']';
+        buffer->commit(sizeof(std::uint32_t)+2);
+        return true;
+    }
+
+    std::uint32_t x;
+};
 
 class custom_log : public reckless::basic_log
 {  
@@ -135,10 +161,45 @@ public:
         <
             reckless::policy_formatter<reckless::indent<4U>, ' '>
         > 
-        (reckless::indent<4U>(), formatting_str, std::forward<ARGS>(args)...);
+        (
+            reckless::indent<4U>(), 
+            formatting_str, 
+            std::forward<ARGS>(args)...
+        );
         // I dont know why I need to default construct an indent, 
-        // but this is how class policy_formatter is implemented.
+        // but this is how it is done in class policy_formatter.
     }
+
+    // ********************************************** //
+    // Why rvalue ref is used for headers? For moving 
+    // if copy : slow
+    // if move : fast
+    // if keep ref only : may dangle before reckless
+    //                    thread writes to file 
+    // ********************************************** //
+    template<typename... ARGS>
+    void log_details(custom_header0&& hdr0,
+//                   custom_header1&& hdr1, 
+//                   custom_header2&& hdr2, 
+                     const char* formatting_str, ARGS&&... args)
+    {
+        reckless::basic_log::write
+        <
+            reckless::policy_formatter<reckless::indent<4U>, ' ', 
+            custom_header0>
+//          custom_header1, 
+//          custom_header2>
+        > 
+        (
+            std::move(hdr0), 
+//          std::move(hdr1), 
+//          std::move(hdr2),
+            reckless::indent<4U>(), 
+            formatting_str, 
+            std::forward<ARGS>(args)...
+        );
+    }
+
 };
 
 void test_reckless_custom_header_and_argument()
@@ -152,11 +213,12 @@ void test_reckless_custom_header_and_argument()
     custom_log logger(&writer);
 
     // Suppose we have an algo doing something, and log arbitrarily ...
-    double m = 3.141592654;
-    double c = 1.2345678e-9;
     logger.log("custom logger name=%s value=%d", "david", 110);
     logger.log("custom logger name=%s value=%d", "susan", 120);
     logger.log("custom logger name=%s value=%d", "frank", 130);
+
+    custom_arg x(110,120,130);
+    logger.log_details(custom_header0(200), "name=%s value=%a", "rgb0", x);
         
 
 }
